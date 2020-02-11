8c26f64d5b28f143cebf70179cf43e0b0f5bd2b6
Harsh <harsh@cybrilla.com>
2020-02-11 23:38:23 +0530

    downloads working

--- imports/collections/Vault.js
+++ imports/collections/Vault.js
@@ -1,2 +1,13 @@
 import { Mongo } from 'meteor/mongo';
 export const Vault = new Mongo.Collection("vault");
+ Vault.insert({
+   serialNo: "HouseTJ",
+   vaultNo: "HouseTJ",
+   shelfNo: "HouseTJ",
+   boxNo: "HouseTJ",
+   weight:1212.11,
+   left: 3232,
+   assignTo: [],
+   membership: "gold",
+   createdAt: new Date()
+ });
